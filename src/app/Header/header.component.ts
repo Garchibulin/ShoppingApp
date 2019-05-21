067436bf09be36b8552b67a7e42c195016c15f80
Ariel Alvarez <aalvarez@cgclatam.com>
2019-05-20 22:49:33 -0600

    commiting local changes

---
+++ src/app/Header/header.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core'
+
+@Component({
+
+    selector: '<shopping-header></shopping-header>',
+    templateUrl: './header.component.html'
+})
+
+export class HeaderComponent{
+    
+}
